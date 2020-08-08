d562880d1b4051a2535ce8c8bf60d8bf7b5024dc
GeoCod <artem.g.vasilchenko@gmail.com>
2020-08-08 13:31:46 +0300

    Дополнительный класс с вспомогательными функциями

---
+++ GB_Algorithms/Verification.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Algorithms
+{
+    /// <summary>Класс вспомогательных функций</summary>
+    public class Verification
+    {
+        /// <summary>Запрос значения "Int" через консоль с проверкой на тип значения</summary>
+        /// <param name="msg">Сообщение, выдаваемое пользователю</param>
+        /// <returns>Введенное в консоль значение</returns>
+        public static int GetInt(string msg)
+        {
+            bool flag; int number;
+            do
+            {
+                flag = Int32.TryParse(Console.ReadLine(), out number);
+                if (!flag)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(msg);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+            } while (!flag);  // Пока false(!false=true), повторять цикл
+            return number;
+        }
+
+        /// <summary>Запрос значения "Double" через консоль с проверкой на тип значения</summary>
+        /// <param name="msg">Сообщение, выдаваемое пользователю</param>
+        /// <returns>Введенное в консоль значение</returns>
+        public static double GetDouble(string msg)
+        {
+            bool flag; double number;
+            do
+            {
+                flag = double.TryParse(Console.ReadLine(), out number);
+                if (!flag)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(msg);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+            } while (!flag);  // Пока false(!false=true), повторять цикл
+            return number;
+        }
+    }
+}
